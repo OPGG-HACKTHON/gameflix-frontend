1dc5805d3864d48503224111b9015333ef6a4ea3
kdongha <kdongha1597@gmail.com>
2021-08-26 02:44:32 +0900

    [#32] theme 정보 localstorage 저장

--- provider/ThemeProvider.tsx
+++ provider/ThemeProvider.tsx
@@ -1,11 +1,19 @@
-import React, { FunctionComponent, useCallback, useState } from 'react';
+import React, { FunctionComponent, useCallback, useEffect, useState } from 'react';
 import { css, Global, ThemeProvider as EmotionThemeProvider } from '@emotion/react';
 import ThemeContext from 'context/theme';
 import { dark, light } from 'themes';
 
 const ThemeProvider: FunctionComponent = (props) => {
     const { children } = props;
-    const [isDark, setIsDark] = useState<boolean>(true);
+    const [isDark, setIsDark] = useState<boolean>(false);
+    useEffect(() => {
+        const isPrevDarkMode = window.localStorage.getItem('theme') === 'dark';
+        const isLocalDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        setIsDark(isPrevDarkMode || isLocalDarkMode);
+    }, []);
+    useEffect(() => {
+        window.localStorage.setItem('theme', isDark ? 'dark' : 'light');
+    }, [isDark]);
     const toggleTheme = useCallback(() => {
         setIsDark((prev) => !prev);
     }, []);
