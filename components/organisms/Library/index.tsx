3848d76a9744ea85906280ff7c39f6039952ead8
kdongha <kdongha1597@gmail.com>
2021-08-31 20:04:59 +0900

    [#37] 라이브러리 가져오기 props 추가

--- components/organisms/Library/index.tsx
+++ components/organisms/Library/index.tsx
@@ -2,30 +2,40 @@ import React, { FunctionComponent } from 'react';
 import { GameInfo } from 'types/responseInterface';
 import styled from '@emotion/styled';
 import ImageSlider from 'components/molecules/ImageSlider';
+import Button from 'components/atoms/Button';
 
 type LibraryProps = {
     label: string;
     list: GameInfo[];
+    onLoad: React.MouseEventHandler<HTMLButtonElement>;
+    loadText?: string;
 };
 
 const Library: FunctionComponent<LibraryProps> = (props) => {
-    const { label, list } = props;
+    const { label, list, loadText = '가져오기', onLoad } = props;
     return (
-        <StyledWrapper>
-            <StyledHeader>{`당신의 ${label} 게임 라이브러리`}</StyledHeader>
+        <LibraryContainer>
+            <LibraryTitle>{`당신의 ${label} 게임 라이브러리`}</LibraryTitle>
+            <Button category={'primary'} onClick={onLoad}>
+                {loadText}
+            </Button>
             <ImageSlider list={list} />
-        </StyledWrapper>
+        </LibraryContainer>
     );
 };
 
 export default Library;
 
-const StyledWrapper = styled.section`
+const LibraryContainer = styled.section`
     padding: 80px;
     width: 100vw;
     margin: 0;
 `;
-const StyledHeader = styled.h2`
+const LibraryTitle = styled.h2`
     color: ${({ theme }) => theme.colors.default};
+    margin-right: 19px;
     margin-bottom: 29px;
+    height: 46px;
+    font-size: 32px;
+    line-height: 46px;
 `;
