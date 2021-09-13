a82b936311e5d9ecc8b75882b28fa60991609e3b
kdongha <kdongha1597@gmail.com>
2021-09-13 20:24:29 +0900

    [#76] 게임 정보보기 페이지 추가 (#77)

--- components/organisms/Header/index.tsx
+++ components/organisms/Header/index.tsx
@@ -54,6 +54,8 @@ const StyledWrapper = styled.header`
     justify-content: space-between;
     padding: 0 80px 0 80px;
     height: 164px;
+    width: 1920px;
+    box-sizing: border-box;
 `;
 
 const StyledHeader = styled.h1`
---
+++ pages/games/[slug].tsx
@@ -0,0 +1,28 @@
+import React, { FunctionComponent } from 'react';
+import { useRouter } from 'next/router';
+import useSWR from 'swr';
+import fetcher from 'utils/fetcher';
+import Error from 'next/error';
+import HomeTemplate from 'components/templates/Home';
+import Header from 'components/organisms/Header';
+import GameDetail from 'components/organisms/GameDetail';
+import { GameInfo } from 'types/responseInterface';
+
+const Game: FunctionComponent = () => {
+    const router = useRouter();
+    const { slug } = router.query;
+    const { data, error } = useSWR<GameInfo>(`/games/${slug}`, fetcher);
+    if (error) {
+        return <Error statusCode={404} />;
+    }
+    if (!data) {
+        return <div>Loading...</div>;
+    }
+    return (
+        <HomeTemplate header={<Header />}>
+            <GameDetail {...data} />
+        </HomeTemplate>
+    );
+};
+
+export default Game;
