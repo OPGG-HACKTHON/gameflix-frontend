40cf62ab6fd5b1fccec845a3add55bbace5863f7
Derek-94 <ghdtjrrl94@gmail.com>
2021-09-16 16:45:33 +0900

    fix: Solved conflict error. (#80)

--- components/atoms/GameImage/index.stories.tsx
+++ components/atoms/GameImage/index.stories.tsx
@@ -10,7 +10,27 @@ export default {
 
 const Template: ComponentStory<typeof GameImage> = (args) => <GameImage {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
-    src: 'https://via.placeholder.com/278x368',
+export const Image = Template.bind({});
+Image.args = {
+    game: {
+        slug: 'league-of-legends',
+        name: 'League of Legend',
+        cover: 'https://images.igdb.com/igdb/image/upload/t_cover_big/co254s.jpg',
+        store: 'steam',
+        release_at: 1256605200,
+        developer: 'riot-games',
+    },
+};
+
+export const ImageWithName = Template.bind({});
+ImageWithName.args = {
+    game: {
+        slug: 'league-of-legends',
+        name: 'League of Legend',
+        cover: 'https://images.igdb.com/igdb/image/upload/t_cover_big/co254s.jpg',
+        store: 'steam',
+        release_at: 1256605200,
+        developer: 'riot-games',
+    },
+    showName: true,
 };
