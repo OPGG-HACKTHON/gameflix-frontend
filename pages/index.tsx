9ee38a03596742e0ba016424c168cd501ef0d8e0
Derek-94 <ghdtjrrl94@gmail.com>
2021-09-16 09:06:44 +0900

    feat(link): Update linking to games/slug. (#85)

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,12 @@ import React, { FunctionComponent } from 'react';
 import HomeTemplate from 'components/templates/Home';
 import Header from 'components/organisms/Header';
 import Libraries from 'components/organisms/Libraries';
+import Footer from 'components/organisms/Footer';
 
 const home: FunctionComponent = () => {
     return (
         <>
-            <HomeTemplate header={<Header />}>
+            <HomeTemplate header={<Header />} footer={<Footer />}>
                 <Libraries />
             </HomeTemplate>
         </>
