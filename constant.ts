584f0071ccb64a7c800f9cadd30b29250ddb6f70
kdongha <kdongha1597@gmail.com>
2021-09-17 05:27:48 +0900

    end point 수정

--- constant.ts
+++ constant.ts
@@ -1,5 +1,4 @@
-export const END_POINT =
-    'http://load-balancer-gameflix-backend-754529149.ap-northeast-2.elb.amazonaws.com';
+export const END_POINT = 'https://www.gameflix-backend.com';
 
 export const GAME_GET_SIZE = 10;
 
